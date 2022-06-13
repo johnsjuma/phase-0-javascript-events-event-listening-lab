9b9c21cb0c2cf15d69d57df13ba06873de923b2b
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-13 19:27:07 +0300

    commit tree b5efd621217fac10d7e41f2983900cdfca95d77e

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function addingEventListener() {
         return "clicked"
       });
     }
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
